ab0904d1ef5b03fa521941c2692437dbff6e9671
James Strachan <james.strachan@gmail.com>
2020-10-16 09:53:56 +0100

    fix: lets connect to the cluster before destroy

--- bin/destroy.sh
+++ bin/destroy.sh
@@ -6,6 +6,10 @@ source `dirname $0`/configure.sh
 
 terraform init
 
+echo "lets connect to the cluster that we are about to destroy so that any helm charts are removed in the right kubernetes cluster..."
+
+$(terraform output connect)
+
 terraform plan -destroy
 
 terraform destroy -auto-approve
